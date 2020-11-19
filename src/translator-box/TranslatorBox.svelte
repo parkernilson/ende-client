1d2ee948b87034325069058ecf367e1bfd6b1e8b
parkernilson <parker.todd.nilson@gmail.com>
2020-11-18 22:19:46 -0700

    improve ui

--- src/translator-box/TranslatorBox.svelte
+++ src/translator-box/TranslatorBox.svelte
@@ -1,68 +1,92 @@
-
 <style>
     .container {
         width: 100%;
-        height: 400px;
-        position: relative;
         box-shadow: 1px 3px 8px rgba(0, 0, 0, 0.3);
         border-radius: 16px;
+        background-color: rgba(246, 246, 246, 1);
     }
 
-    .text-input {
-        width: 100%;
-        height: 50%;
-        position: relative;
-        border-radius: 16px;
-        margin-bottom: -25%;
-        z-index: 1000;
-        color: black;
+    .input-container {
+        display: flex;
+        flex-direction: column;
+
+        padding: 16px;
         box-shadow: 1px 3px 8px rgba(0, 0, 0, 0.3);
+        background-color: white;
+        border-radius: 16px;
     }
 
     .text-input-area {
         width: 100%;
         height: 100%;
-        position: relative;
-        border-radius: 16px;
-        resize: none;
-        padding: 16px;
-    }
-
-    .translation-output-container {
-        background-color: rgba(246, 246, 246, 1);
-        position: absolute;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        border-radius: 16px;
-    }
-
-    .translation-output {
-        border: none;
-        width: 100%;
-        height: 50%;
-        margin: 0;
-        position: absolute;
-        bottom: 0;
-        padding: 16px;
         box-sizing: border-box;
+        margin-bottom: 6px;
+
         color: black;
+        border-radius: 16px;
+        background-color: rgba(0, 0, 0, 0);
+        border: none;
+    }
+
+    .flex-row {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
     }
 
     .translate-button {
-        position: absolute;
         height: 32px;
+        margin: 0;
         border-radius: 16px;
         background-color: rgba(187, 127, 255, 1);
         box-shadow: 1px 3px 8px rgba(0, 0, 0, 0.3);
         border: none;
         color: white;
-        right: 16px;
-        bottom: 8px;
+    }
+
+    .output-container {
+        border-bottom-left-radius: 16px;
+        border-bottom-right-radius: 16px;
+        padding: 16px;
+    }
+
+    .translation-output {
+        border: none;
+        width: 100%;
+        margin: 0;
+        box-sizing: border-box;
+        color: black;
+    }
+
+    [contenteditable]:active,
+    [contenteditable]:focus {
+        border:none;
+        outline:none;
+    }
+
+    .text-input-area:empty::before {
+        content: attr(placeholder);
+        color: gray;
+    }
+
+    .text-input-area:empty:focus::before {
+        content: "";
+    }
+
+    .char-count {
+        color: black;
+    }
+
+    .alert {
+        color: red;
     }
 </style>
 
 <script lang="typescript">
+    import { slide } from 'svelte/transition';
+
+    const maxInputLength = 300;
+
     /** value of the input text for translation */
     let inputText: string = "";
     /** value of the last translated phrase */
@@ -72,6 +96,11 @@
      * Request a translation from the translation server
      */
     function requestTranslation(phrase: string) {
+        if (inputText.length > maxInputLength) {
+            alert(`The max input length is ${maxInputLength} characters`);
+            return;
+        }
+
         // indicate loading
         translatedText = "loading translation...";
 
@@ -101,11 +130,20 @@
 </script>
 
 <div class="container">
-    <div class="text-input">
-        <textarea bind:value={inputText} placeholder="Type a phrase you would like to translate here..." class="text-input-area" maxlength="300"/>
-        <button class="translate-button" on:click={()=>requestTranslation(inputText)}>Translate</button>
+    <div class="input-container">
+        <div contenteditable bind:textContent={inputText} placeholder="Type a phrase you would like to translate here..." class="text-input-area">
+            {inputText}
+        </div>
+        <div class="flex-row">
+            <div class="char-count {inputText.length > maxInputLength && "alert"}">{inputText.length || 0}/{maxInputLength}</div>
+            <button class="translate-button" on:click={()=>requestTranslation(inputText)}>Translate</button>
+        </div>
     </div>
-    <div class="translation-output-container">
-        <p class="translation-output">{translatedText}</p>
-    </div>
+    {#if translatedText !== ""}
+        {#key translatedText}
+            <div transition:slide class="output-container">
+                <p class="translation-output">{translatedText}</p>
+            </div>
+        {/key}
+    {/if}
 </div>
